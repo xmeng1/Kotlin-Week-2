9936728d1b22e9fc2dc406b2f53d722197f364c4
Xin Meng <x.meng@outlook.com>
2019-09-14 07:19:01 +0100

    update to stream solution

--- Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -1,9 +1,23 @@
 package mastermind
 
+import kotlin.math.min
+
 
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
+
+    val rightPositions = secret.zip(guess).count {
+        it.first == it.second
+    }
+
+    val commonLetters = "ABCDEF".sumBy { ch ->
+        Math.min(secret.count { it ==ch }, guess.count { it == ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
+}
+
+fun evaluateGuess2(secret: String, guess: String): Evaluation {
     val length = secret.length
     var right = 0
     val rightPositionArray = BooleanArray(length) { _ -> false }
